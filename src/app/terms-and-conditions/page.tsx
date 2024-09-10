df4df3455f2461f4674c51acc47dea6255226040
kervengully <gullyyevv@gmail.com>
2024-09-10 16:19:24 +0300

    1

--- src/app/terms-and-conditions/page.tsx
+++ src/app/terms-and-conditions/page.tsx
@@ -11,7 +11,7 @@ const page: React.FC<pageProps> = ({ }) => {
         <h1 className='font-bold'>Privacy Policy</h1>
 
         <div className='mt-8 space-y-4'>
-          <h3 className='font-semibold'>Inspired Online Schools Privacy Notice</h3>
+          <h3 className='font-semibold'>Online Tutoring Courses Privacy Notice</h3>
           <div>
             <p>
               {"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
